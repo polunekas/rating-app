3ed696ef1cb2a20618f86781e15e5be8e5a2e0ef
polina <polune.kas@gmail.com>
2023-07-07 20:16:59 +0300

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import { AppProps } from 'next/app'
+import '../styles/globals.css'
+import Head from 'next/head'
+import '../styles/globals.css'
+import React from 'react'
+
+function MyApp({ Component, pageProps }:AppProps): JSX.Element {
+  return <>
+	<Head>
+		<title>Mytop</title>
+		<link rel="preconnect" href="https://fonts.googleapis.com"/>
+		<link rel="preconnect" href="https://fonts.gstatic.com" />
+		<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&family=Open+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+	</Head>
+		<Component {...pageProps} />
+  </>
+}
+
+export default MyApp
